2829178cbe8ead9607e90ed109976301fe0ac317
cipherbeast03 <88836313+cipherbeast03@users.noreply.github.com>
2023-08-03 02:04:03 +0530

    4 files modified, 4 added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 //what does this code line mean "
 import React from 'react';
+import './style.css';
 
 function App() {
   return (
-    <div>
-      <h1>Working</h1>
+    <div className="container">
+      <div className="product">Product Name</div>
+      <div className="quantity">
+        <button className="btn" id="decrementBtn">
+          -
+        </button>
+        <div className="quantity-value" id="quantityValue">
+          1
+        </div>
+        <button className="btn" id="incrementBtn">
+          +
+        </button>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import store from './store';
 
 import App from './App';
 
+store.subscribe(() => console.log(store.getState()));
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
