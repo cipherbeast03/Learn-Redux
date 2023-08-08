94b9b13f35356ccfd44b66723dda2e2a9dfe768a
cipherbeast03 <88836313+cipherbeast03@users.noreply.github.com>
2023-08-08 23:25:30 +0530

    1 file modified

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import store from './store';
+import { Provider } from 'react-redux';
 
 import App from './App';
 
@@ -11,6 +12,8 @@ const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
